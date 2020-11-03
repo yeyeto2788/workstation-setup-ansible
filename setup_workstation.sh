3c6908205911eaefc9d7f423e2dc59691e707d25
yeyeto2788 <juanernestobiondi@gmail.com>
2020-11-03 14:16:51 +0100

    Add proper syntax and exit code.

--- setup_workstation.sh
+++ setup_workstation.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/bash
-#apt update 
-#apt upgrade -y
-#apt install python3 python3-pip curl git -y
-#cd ~
-#git clone https://github.com/yeyeto2788/workstation-setup-ansible.git
-#cd ./workstation-setup-ansible
-#pip3 install ansible
-ansible-playbook main.yaml -K
+#!/bin/bash
+apt update 
+apt upgrade -y
+apt install python3 python3-pip curl git -y
+cd ~
+git clone https://github.com/yeyeto2788/workstation-setup-ansible.git
+cd ./workstation-setup-ansible
+pip3 install ansible
+ansible-playbook main.yaml --syntax-check
+#
+echo "Press [ENTER] to terminate and delete files."
+read -s -N 1 -t 1 key
+if [[ $key == $'\x0a' ]];        # if input == ENTER key
+then
+cd ~ 
+rm -rf ./workstation-setup-ansible
+exit 0
+fi
+
