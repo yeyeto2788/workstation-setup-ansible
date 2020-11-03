70999f0d68db64aff14e6d331ac9cf8bd3c0461a
yeyeto2788 <juanernestobiondi@gmail.com>
2020-11-03 16:58:54 +0100

    Remove not needed parts.

--- setup_workstation.sh
+++ setup_workstation.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-apt update
-apt upgrade -y
-apt install python3 python3-pip curl git -y
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3 python3-pip curl git -y
 cd ~
 git clone https://github.com/yeyeto2788/workstation-setup-ansible.git
 cd ./workstation-setup-ansible
-pip3 install ansible
+sudo pip3 install ansible
 ansible-galaxy install --roles-path ./roles -r ./requirements.yaml
 ansible-playbook main.yaml -K
-#
-echo "Press [ENTER] to terminate and delete files."
-while [ true ] ; do
-    read -s -N 1 -t 1 key
-    if [[ $key == $'\x0a' ]];   # if input == ENTER key
-    then
-        cd ~
-        rm -rf ./workstation-setup-ansible
-        exit 0
-    fi
-done
+
+read -n 1 -s -r -p "Press [ENTER] to terminate and delete files."
+cd ~
+rm -rf ./workstation-setup-ansible
+exit 0
